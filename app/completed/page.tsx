0429c7d0c4900356cda00deb3e9df2ef064705c0
yunusabdul <yunusabdulmajidyunus38@gmail.com>
2025-01-27 03:56:09 +0100

    fix: workout completed ui

--- app/completed/page.tsx
+++ app/completed/page.tsx
@@ -1,3 +1,31 @@
-export default function CompletedPage() {
-    return null;
-  }
+import Workout from "@/components/workout";
+import Box from "@mui/material/Box";
+
+export default function WorkoutDetailsPage() {
+  const data = {
+    progress: 70,
+    // duration:0,
+    completed: 6,
+    // calories:0,
+    // weight:0,
+    // tutAccuracy:0,
+  };
+
+  return (
+    <Box
+      sx={{
+        minHeight: "100vh",
+        bgcolor: "white",
+        color: "black",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        px: 3,
+        py: 5,
+        justifyItems: "center",
+      }}
+    >
+      <Workout progress={data.progress} completed={data.completed} />
+    </Box>
+  );
+}
