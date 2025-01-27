06ada841eba7b1bea0256d726419d4a5f858e5aa
yunusabdul <yunusabdulmajidyunus38@gmail.com>
2025-01-27 03:26:12 +0100

    updated workouts

--- app/workouts/page.tsx
+++ app/workouts/page.tsx
@@ -1,10 +1,21 @@
-"use client"
+"use client";
 
-import React from 'react';
-import { useRouter } from 'next/navigation'; 
-import { workoutLists } from '../Mocks/WorkoutsList';
-import { WorkoutContainer } from '@/components/Containers/WorkoutContainer';
+import React from "react";
+import { useRouter } from "next/navigation";
+import { workoutLists } from "../Mocks/WorkoutsList";
+import { WorkoutContainer } from "@/components/Containers/WorkoutContainer";
 
 export default function WorkoutsPage() {
-    return null;
-  }
+  const router = useRouter();
+  const allWorkouts = Object.values(workoutLists).flat();
+
+  const handleWorkoutSelected = () => {
+    router.push("/workouts/details");
+  };
+  return (
+    <WorkoutContainer
+      workouts={allWorkouts}
+      handleWorkoutSelected={handleWorkoutSelected}
+    />
+  );
+}
--- app/workouts[id]/page.tsx
+++ app/workouts[id]/page.tsx
@@ -1,3 +1,32 @@
+import Workout from "@/components/workout";
+import Box from "@mui/material/Box";
+
 export default function WorkoutDetailsPage() {
-    return null;
+
+  const data = {
+    progress: 70,
+    // duration:0,
+    completed: 6,
+    // calories:0,
+    // weight:0,
+    // tutAccuracy:0,
+  };
+
+      return (
+        <Box
+          sx={{
+            minHeight: "100vh",
+            bgcolor: "white",
+            color: "black",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            px: 3,
+            py: 5,
+            justifyItems: "center",
+          }}
+        >
+          <Workout progress={data.progress} completed={data.completed} />
+        </Box>
+      );
   }
