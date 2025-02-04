bfc126b43ffb4ba2b3e415987f49529f587e2900
Joel Vargas <vermudo.com@gmail.com>
2025-02-04 17:23:42 -0600

    refactor: simplify WorkoutsPage by removing unused router and streamlining workout data handling

--- src/app/home/workouts/page.tsx
+++ src/app/home/workouts/page.tsx
@@ -1,21 +1,10 @@
 "use client";
 
 import React from "react";
-import { useRouter } from "next/navigation";
-import { workoutLists } from "@/src/constants/mocks/WorkoutsList";
+import { workoutList } from "@/src/constants/mocks/WorkoutsList";
 import { WorkoutContainer } from "@/src/components/Containers/WorkoutContainer";
 
 export default function WorkoutsPage() {
-  const router = useRouter();
-  const allWorkouts = Object.values(workoutLists).flat();
-
-  const handleWorkoutSelected = () => {
-    router.push("/home/workouts/details");
-  };
-  return (
-    <WorkoutContainer
-      workouts={allWorkouts}
-      handleWorkoutSelected={handleWorkoutSelected}
-    />
-  );
+  const allWorkouts = Object.values(workoutList).flat();
+  return <WorkoutContainer workouts={allWorkouts} />;
 }
