145f2c5bd452f6edfd54f6ce915690283a438a16
Joel Vargas <vermudo.com@gmail.com>
2025-02-04 17:24:24 -0600

    feat: update WorkoutCard to use Link for navigation and improve layout

--- src/components/Cards/WorkoutCard.tsx
+++ src/components/Cards/WorkoutCard.tsx
@@ -3,89 +3,93 @@
 import React from "react";
 import { Box, Typography, IconButton } from "@mui/material";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import Link from "next/link";
 
 interface WorkoutCardProps {
+  id: string;
   name: string;
   description: string;
   image: string;
-  handleWorkoutSelected: () => void;
 }
 
 const WorkoutCard: React.FC<WorkoutCardProps> = ({
+  id,
   name,
   description,
   image,
-  handleWorkoutSelected,
 }) => {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-        textAlign: "center",
-        backgroundColor: "#2B2A2D",
-        borderRadius: "15px",
-      }}
-    >
-      <Box sx={{ display: "flex", alignItems: "center" }}>
-        <img
-          src={image}
-          alt={name}
-          style={{
-            width: "30%",
-            height: "100%",
-            borderRadius: "10px",
-          }}
-        />
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "flex-start",
-            width: "60%",
-            px: 2,
-          }}
-        >
-          <Typography
-            variant="h6"
-            sx={{ fontWeight: 600, fontSize: "16px", mb: 1 }}
-          >
-            {name}
-          </Typography>
-          <Typography
-            variant="body2"
-            sx={{ color: "gray", fontSize: "14px", mb: 1 }}
-          >
-            {description}
-          </Typography>
-        </Box>
-        <div className="w-1/12">
-          <IconButton
-            onClick={handleWorkoutSelected}
+    <Link href={`/home/workouts/details/${id}`}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          textAlign: "center",
+          backgroundColor: "#2B2A2D",
+          borderRadius: "15px",
+        }}
+      >
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <img
+            src={image}
+            alt={name}
+            style={{
+              width: "100px",
+              height: "100px",
+              borderRadius: "10px",
+              padding: "10px",
+            }}
+          />
+          <Box
             sx={{
-              marginLeft: "auto",
-              backgroundColor: "#F7F7F7",
-              borderRadius: "50%",
-              boxShadow: "0px 4px 6px #00729840",
-              "&:hover": {
-                backgroundColor: "#F7F7F7",
-              },
-              width: "28px",
-              height: "28px",
               display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              p: 2,
-              marginRight: "15px",
+              flexDirection: "column",
+              alignItems: "flex-start",
+              width: "60%",
+              px: 2,
             }}
           >
-            <ArrowForwardIosIcon sx={{ fontSize: 20, color: "black" }} />
-          </IconButton>
-        </div>
+            {/* Please, add a kind of truncate y description... */}
+            <Typography
+              variant="h6"
+              sx={{ fontWeight: 600, fontSize: "16px", mb: 1 }}
+            >
+              {name}
+            </Typography>
+            <Typography
+              variant="body2"
+              sx={{ color: "gray", fontSize: "14px", mb: 1, textAlign: "left" }}
+            >
+              {description}
+            </Typography>
+          </Box>
+          <div className="w-1/12">
+            <IconButton
+              sx={{
+                marginLeft: "auto",
+                backgroundColor: "#F7F7F7",
+                borderRadius: "50%",
+                boxShadow: "0px 4px 6px #00729840",
+                "&:hover": {
+                  backgroundColor: "#F7F7F7",
+                },
+                width: "28px",
+                height: "28px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                p: 2,
+                marginRight: "15px",
+              }}
+            >
+              <ArrowForwardIosIcon sx={{ fontSize: 20, color: "black" }} />
+            </IconButton>
+          </div>
+        </Box>
       </Box>
-    </Box>
+    </Link>
   );
 };
 
