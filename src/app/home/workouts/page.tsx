91f73a88916b3dbb19c8b6541337bd521d0f534f
Joel Vargas <vermudo.com@gmail.com>
2025-02-04 17:25:07 -0600

    fix: update import path for WorkoutContainer in WorkoutsPage component

--- src/app/home/workouts/page.tsx
+++ src/app/home/workouts/page.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import { workoutList } from "@/src/constants/mocks/WorkoutsList";
-import { WorkoutContainer } from "@/src/components/Containers/WorkoutContainer";
+import { WorkoutContainer } from "@/src/components/layout/containers/WorkoutContainer";
 
 export default function WorkoutsPage() {
   const allWorkouts = Object.values(workoutList).flat();
