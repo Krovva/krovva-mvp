9690d388e48a5ac49a1d75d62f2e5dd59d1156c1
Joel Vargas <vermudo.com@gmail.com>
2025-02-04 17:24:08 -0600

    feat: add WorkoutDetailsPage component to display individual workout details

---
+++ src/app/home/workouts/completed/[workoutId]/page.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import Workout from "@/src/components/workout";
+import { workoutList } from "@/src/constants/mocks/WorkoutsList";
+import Box from "@mui/material/Box";
+import { useParams } from "next/navigation";
+
+export default function WorkoutDetailsPage() {
+  const data = {
+    progress: 70,
+    // duration:0,
+    completed: 6,
+    // calories:0,
+    // weight:0,
+    // tutAccuracy:0,
+  };
+
+  const { workoutId } = useParams();
+
+  const workout = workoutList.find((w) => w.id === workoutId);
+
+  if (!workout) {
+    return <p>Workout not found</p>;
+  }
+
+  return (
+    <Box
+      sx={{
+        minHeight: "100vh",
+        bgcolor: "white",
+        color: "black",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        px: 3,
+        py: 5,
+        justifyItems: "center",
+      }}
+    >
+      <Workout
+        progress={data.progress}
+        completed={data.completed}
+        workout={workout}
+      />
+    </Box>
+  );
+}
