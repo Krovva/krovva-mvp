751255e5e56c36e358e0eecaabda108ba70037ef
Joel Vargas <vermudo.com@gmail.com>
2025-02-04 11:41:18 -0600

    feat: implement Privacy Policy and Terms of Service pages with basic structure

--- src/app/home/terms-service/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function TermsServicePage() {
-  return null;
-}
---
+++ src/app/terms-service/page.tsx
@@ -0,0 +1,9 @@
+const TermsOfServicePage = () => {
+  return (
+    <div>
+      <h1 className="text-primary-600">TermsOfServicePage</h1>
+    </div>
+  );
+};
+
+export default TermsOfServicePage;
