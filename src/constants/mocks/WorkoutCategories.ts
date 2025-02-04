dfe7ed78ae151c0b8241809916e86600a049d770
Joel Vargas <vermudo.com@gmail.com>
2025-02-04 17:23:04 -0600

    feat: add mock workout categories for improved organization and categorization

---
+++ src/constants/mocks/WorkoutCategories.ts
@@ -0,0 +1,8 @@
+export const categories = [
+  { name: "yoga", image: "/Categories/yoga.png" },
+  { name: "gym", image: "/Categories/lifting.png" },
+  { name: "cardio", image: "/Categories/cardio.png" },
+  { name: "stretch", image: "/Categories/stretch.png" },
+  { name: "full Body", image: "/Categories/fullbody.png" },
+  { name: "legs", image: "/Categories/legs.png" },
+];
