924e7b8ee7f761a536acef343df6d2d80781149c
Joel Vargas <vermudo.com@gmail.com>
2025-02-04 12:18:16 -0600

    feat: add page data constants for header items with titles and subtitles

---
+++ src/constants/phrases/header-items.ts
@@ -0,0 +1,27 @@
+type PageInfo = {
+  subtitle: string;
+  title: string;
+};
+
+export const pageData: Record<string, PageInfo> = {
+  home: {
+    subtitle: "EVERYDAY WE'RE MUSCLE'N",
+    title: "Hello, Kakashi 👋",
+  },
+  profile: {
+    subtitle: "Your journey, your rules!",
+    title: "Profile",
+  },
+  workouts: {
+    subtitle: "Train insane or remain the same!",
+    title: "Workouts",
+  },
+  nutrition: {
+    subtitle: "Fuel your body, fuel your goals!",
+    title: "Nutrition",
+  },
+  settings: {
+    subtitle: "Customize your experience!",
+    title: "Settings",
+  },
+};
